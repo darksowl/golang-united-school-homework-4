d7eeaf7434095380101e2f71ae83e6d77624be41
Natalya <trotsyuknata@yandex.ru>
2022-05-29 20:46:41 +0300

    homework

--- string_sum.go
+++ string_sum.go
@@ -1,7 +1,12 @@
 package string_sum
 
+//package main
+
 import (
 	"errors"
+	//"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +28,44 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+
+	if len(input) == 0 {
+		return output, fmt.Errorf("%w", errorEmptyInput)
+	}
+	n := make([]int, 2)
+	var e error
+	m := make([]string, 2)
+	j := 0
+	for i := 1; i < len(input); i++ {
+		if string(input[i]) == "+" || string(input[i]) == "-" {
+			m[0] = input[j:i]
+			j = i
+			break
+		}
+	}
+	//fmt.Println(m[0])
+	n[0], e = strconv.Atoi(strings.ReplaceAll(m[0], " ", ""))
+	//fmt.Println(n[0],e)
+	m[1] = input[j:len(input)]
+	m[1] = strings.ReplaceAll(m[1], " ", "")
+	//fmt.Println(m[1])
+	for i := 1; i < len(m[1]); i++ {
+		if string(m[1][i]) == "+" || string(m[1][i]) == "-" {
+			return output, fmt.Errorf("%w", errorNotTwoOperands)
+		}
+	}
+	n[1], e = strconv.Atoi(m[1])
+	if e != nil {
+		return output, fmt.Errorf("%w", errorNotTwoOperands)
+	}
+	//fmt.Println(n[1],e)
+	sum := n[0] + n[1]
+	output = strconv.Itoa(sum)
+	return output, nil
 }
+
+//func main(){
+//	s := "- 54321 - "
+//	a,err := StringSum(s)
+//	fmt.Printf("%s,%v",a,err)
+//}
